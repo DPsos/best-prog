4a48c06ebd6fb876a5098e4964f05f56c7dffa99
DPsos <108731221+DPsos@users.noreply.github.com>
2022-09-03 21:11:49 +0300

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<form action="send.php" method="post">
-    <input type="text" name="login" required>
-    <input type="text" name="message" required>
-
-    <input type="submit" value="Отправить Данные">
-</form>
